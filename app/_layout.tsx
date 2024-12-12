41c68fb05953f4f1aed257e2f16c7ca349795fd4
Matias Ongay <ongaymatias@gmail.com>
2024-12-12 15:16:56 -0300

    Expo router

---
+++ app/_layout.tsx
@@ -0,0 +1,12 @@
+import { Slot } from "expo-router";
+import { View } from "react-native";
+
+export default function Layout() {
+
+
+  return (
+    <View>
+        <Slot></Slot>
+    </View>
+  );
+}
