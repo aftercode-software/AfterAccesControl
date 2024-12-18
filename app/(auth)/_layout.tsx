39f257ef6b1305e75bd0134a93b356b2d90353fc
Geer68 <germhidalgo@gmail.com>
2024-12-18 10:44:08 -0300

    mio

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,6 +1,5 @@
 import { Slot } from "expo-router";
 
 export default function LayoutLogin() {
-    
-    return <Slot/>
+  return <Slot />;
 }
--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -15,15 +15,15 @@ export default function Login() {
     throw new Error("AuthContext no está disponible.");
   }
 
-  const emailRef = useRef<string>(""); // Referencia para el email
+  const usernameRef = useRef<string>(""); // Referencia para el nombre de usuario
   const passwordRef = useRef<string>(""); // Referencia para la contraseña
   const [loading, setLoading] = useState(false);
 
   const handleLogin = async () => {
-    const email = emailRef.current.trim();
+    const username = usernameRef.current.trim();
     const password = passwordRef.current.trim();
 
-    if (!email || !password) {
+    if (!username || !password) {
       Alert.alert("Error", "Por favor, completa todos los campos.");
       return;
     }
@@ -32,20 +32,22 @@ export default function Login() {
     try {
       const response = await axios.post(
         "https://backend-afteraccess.vercel.app/login",
-        { email, password }
+        { username, password }
       );
 
+      Alert.alert(response.data.message);
       const { id, token } = response.data;
 
       // Guardar el token de forma segura
       await SecureStore.setItemAsync("userToken", token);
 
       // Actualizar el contexto con el usuario
-      setUser({ id, email, token });
+      setUser({ id, username, token });
       Alert.alert("Éxito", "Inicio de sesión exitoso.");
     } catch (error: any) {
       const errorMessage =
-        error.response?.data?.message || "No se pudo iniciar sesión. Verifica tus credenciales.";
+        error.response?.data?.message ||
+        "No se pudo iniciar sesión. Verifica tus credenciales.";
       Alert.alert("Error", errorMessage);
     } finally {
       setLoading(false);
@@ -54,35 +56,39 @@ export default function Login() {
 
   return (
     <GluestackUIProvider mode="light">
-      <View className="flex items-center justify-center h-full">
-        <FormControl className="p-4 border rounded-lg border-outline-300">
+      <View className="flex items-center justify-center h-full p-6">
+        <FormControl className="w-full max-w-lg p-6 border rounded-lg border-outline-300">
           <VStack space="xl">
             <VStack space="xs">
-              <Text className="text-typography-500">Email</Text>
-              <Input className="min-w-[250px]">
+              <Text className="text-2xl font-semibold text-typography-500">
+                Username
+              </Text>
+              <Input className="w-full min-h-12">
                 <InputField
                   type="text"
-                  onChangeText={(text) => (emailRef.current = text)}
-                  placeholder="Ingresa tu email"
+                  placeholder="Ingresa tu nombre de usuario"
+                  onChangeText={(text) => (usernameRef.current = text)}
                 />
               </Input>
             </VStack>
             <VStack space="xs">
-              <Text className="text-typography-500">Password</Text>
-              <Input className="min-w-[250px]">
+              <Text className="text-2xl font-semibold text-typography-500">
+                Password
+              </Text>
+              <Input className="w-full min-h-12">
                 <InputField
                   type="password"
-                  onChangeText={(text) => (passwordRef.current = text)}
                   placeholder="Ingresa tu contraseña"
+                  onChangeText={(text) => (passwordRef.current = text)}
                 />
               </Input>
             </VStack>
             <Button
-              className="ml-auto"
+              className="w-full py-4 mt-4 min-h-16"
               onPress={handleLogin}
               disabled={loading}
             >
-              <ButtonText className="text-typography-0">
+              <ButtonText className="text-xl font-bold text-typography-0">
                 {loading ? "Cargando..." : "Iniciar sesión"}
               </ButtonText>
             </Button>
